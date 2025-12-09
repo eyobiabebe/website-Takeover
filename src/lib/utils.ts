0b807c90720a38b55e1bfcb9f3cadccd81f88481
Abdulsomed Jibril <chuchuchacha151@gmail.com>
2025-12-09 22:15:54 +0300

    edditing12

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export async function checkProfileComplete(userId: string) {
 
-  const res = await axios.post('/api/profile', { userId }, { withCredentials: true });
+  const res = await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/profile`, { userId }, { withCredentials: true });
 
   return await res.data.profile.isCompleted;
 }
